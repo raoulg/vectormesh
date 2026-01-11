09449b2b3000fbd92c8fea0d8486973127870f27
raoul grouls <Raoul.Grouls@han.nl>
2026-01-11 10:42:50 +0100

    fix forward, add RNN aggregator

--- src/vectormesh/components/aggregation.py
+++ src/vectormesh/components/aggregation.py
@@ -1,17 +1,23 @@
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
+import torch
+import torch.nn as nn
 from beartype import beartype
 from jaxtyping import Float, jaxtyped
 from torch import Tensor
-from torch.nn.utils.rnn import pad_sequence
 
 
-class BaseAggregator:
-    """Base class for aggregating 3D -> 2D tensors."""
+class BaseAggregator(ABC, nn.Module):
+    """Base class for aggregating 3D -> 2D tensors.
+    We use "forward" to be compatible with nn.Module
+    """
+
+    def __init__(self):
+        super().__init__()
 
     @abstractmethod
     @jaxtyped(typechecker=beartype)
-    def __call__(
+    def forward(
         self, embeddings: Float[Tensor, "batch _ dim"]
     ) -> Float[Tensor, "batch dim"]:
         """Aggregate from (batch, chunks, dim) to (batch, dim)."""
@@ -19,23 +25,50 @@ class BaseAggregator:
 
 
 class MeanAggregator(BaseAggregator):
-    """Aggregate by taking mean over chunks."""
+    """Aggregate by taking mean over chunks.
+    no learnable parameters.
+    """
 
     @jaxtyped(typechecker=beartype)
-    def __call__(
+    def forward(
         self, embeddings: Float[Tensor, "batch _ dim"]
     ) -> Float[Tensor, "batch dim"]:
         """Mean over chunks dimension."""
         return embeddings.mean(dim=1)
 
 
-class DynamicPadding:
+class AttentionAggregator(BaseAggregator):
+    """Aggregate using learnable attention over chunks."""
+
+    def __init__(self, hidden_size: int):
+        """initialize learnable parameters."""
+        super().__init__()
+        self.attention = nn.Linear(hidden_size, 1)
+
     @jaxtyped(typechecker=beartype)
-    def __call__(
-        self, embeddings: list[Float[Tensor, "chunks dim"]]
-    ) -> Float[Tensor, "batch max_chunks dim"]:
-        """Pad sequences to the maximum length in the batch."""
-        return pad_sequence(
-            embeddings,
-            batch_first=True,
+    def forward(
+        self, embeddings: Float[Tensor, "batch _ dim"]
+    ) -> Float[Tensor, "batch dim"]:
+        # attention_weights: (batch, _, 1)
+        attention_weights = torch.softmax(self.attention(embeddings), dim=1)
+        return (embeddings * attention_weights).sum(dim=1)
+
+
+class RNNAggregator(BaseAggregator):
+    """Aggregate using RNN over chunks.
+    return final hidden state.
+    """
+
+    def __init__(self, hidden_size: int):
+        """initialize learnable parameters."""
+        super().__init__()
+        self.rnn = torch.nn.GRU(
+            input_size=hidden_size, hidden_size=hidden_size, batch_first=True
         )
+
+    @jaxtyped(typechecker=beartype)
+    def forward(
+        self, embeddings: Float[Tensor, "batch _ dim"]
+    ) -> Float[Tensor, "batch dim"]:
+        output, _ = self.rnn(embeddings)
+        return output[:, -1, :]
