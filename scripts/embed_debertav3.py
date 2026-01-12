4d0bd845b7c26282b907393ade8ed08339e0dbb1
raoul grouls <Raoul.Grouls@han.nl>
2026-01-12 13:50:52 +0100

    deberta script

---
+++ scripts/embed_debertav3.py
@@ -0,0 +1,48 @@
+import sys
+from pathlib import Path
+
+from datasets import Dataset, load_from_disk
+from loguru import logger
+
+from vectormesh import VectorCache, Vectorizer
+
+logger.remove()
+logger.add(sys.stderr, level="INFO")
+logger.add("logs/embed_deberta.log", rotation="10 MB", level="DEBUG")
+
+
+def main():
+    """
+    microsoft/deberta-v3-small:
+        DeBERTa v3 - DeBERTa-v3-large outperforms on five out of nine tasks in the
+        Dutch Model Benchmark Clinjournal, even though it's technically English.
+        DeBERTa uses more sophisticated positional encoding and attention mechanisms
+        than BERT/RoBERTa. There's also mDeBERTa (multilingual DeBERTa) which includes Dutch.
+    """
+    assets = Path("assets")
+    if not assets.exists():
+        logger.error(f"Assets folder not found at: {assets.resolve()}")
+        raise FileNotFoundError(f"Assets folder not found at: {assets.resolve()}")
+    tag = next(assets.glob("aktes_*/"))
+    trainpath = tag / "train"
+    if not trainpath.exists():
+        logger.error(f"Train dataset not found at: {trainpath}")
+        raise FileNotFoundError(f"Train dataset not found at: {trainpath}")
+    logger.info(f"Loading data from: {trainpath}")
+
+    train = load_from_disk(trainpath)
+    assert type(train) is Dataset
+    model_name = "microsoft/deberta-v3-small"
+    vectorizer = Vectorizer(model_name=model_name, col_name="debertav3-small")
+
+    vectorcache = VectorCache.create(
+        cache_dir=Path("artefacts"),
+        vectorizer=vectorizer,
+        dataset=train,
+        dataset_tag=tag.name,
+    )
+    logger.success(f"Created vector cache at: {vectorcache.cache_dir}")
+
+
+if __name__ == "__main__":
+    main()
