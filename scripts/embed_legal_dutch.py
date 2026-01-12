5ecb1d2fda8cc694be1bc0efd03cea7f9c497141
raoul grouls <Raoul.Grouls@han.nl>
2026-01-12 10:50:58 +0100

    embed script

---
+++ scripts/embed_legal_dutch.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from datasets import load_from_disk
+from loguru import logger
+
+from vectormesh import VectorCache, Vectorizer
+
+logger.remove()
+logger.add(sys.stderr, level="INFO")
+logger.add("logs/embed_legal_dutch.log", rotation="10 MB", level="DEBUG")
+
+
+def main():
+    assets = Path("../assets")
+    tag = next(assets.glob("aktes_*/"))
+    trainpath = tag / "train"
+    logger.info(f"Loading data from: {trainpath}")
+
+    train = load_from_disk(trainpath)
+    model_name = "Gerwin/legal-bert-dutch-english"
+    vectorizer = Vectorizer(model_name=model_name, col_name="legal_dutch")
+
+    vectorcache = VectorCache.create(
+        cache_dir=Path("../artefacts"),
+        vectorizer=vectorizer,
+        dataset=train,
+        dataset_tag=tag.name,
+    )
+    logger.success(f"Created vector cache at: {vectorcache.cache_dir}")
